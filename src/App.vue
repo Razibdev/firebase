f42be3e28da71a53623b39303ae6359daaf2b697
Razib Hossen <razibhossendev@gmail.com>
2022-08-02 01:37:49 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld :posts="posts" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import getPosts from './components/composables/getPosts';
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  setup(){
+    const {posts, error, load} = getPosts();
+    load();
+    console.log(posts);
+
+    return {posts, error, load};
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyBCMS8qtUrLIypdb6Al1oQbNb5Pv9HYHxs",
+  authDomain: "test-50589.firebaseapp.com",
+  projectId: "test-50589",
+  storageBucket: "test-50589.appspot.com",
+  messagingSenderId: "818741521647",
+  appId: "1:818741521647:web:b08edc60f9640538dfff1a",
+};
+
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init firestore service
+const projectFirestore = firebase.firestore();
+export { projectFirestore};
